124248e0179bfa56338ef10d026d38cc6eab2398
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:17:49 -0700

    Slightly more elegant.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,17 +1,16 @@
+
 import sys
 
 def ratings_output():
     """ Reads given file, then spits out the ratings in alphabetical order by restaurant. 
-
     """
 
     restaurant_data = open(sys.argv[1])
     zagat = {}
     for line in restaurant_data:
-        line = line.rstrip().split(":")
-        zagat[line[0]] = line[1]
-    zagat_sorted = sorted(zagat)
-    for restaurant in zagat_sorted:
-        print "{} is rated at {}.".format(restaurant , zagat[restaurant])
+        restaurant, score = line.rstrip().split(":")
+        zagat[restaurant] = score
+    for restaurant in sorted(zagat):
+        print "{} is rated at {}.".format(restaurant, zagat[restaurant])
 
 ratings_output()
