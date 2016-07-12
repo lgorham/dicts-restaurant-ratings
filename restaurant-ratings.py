0f4e8ec346c4a9eba26bff87c6ae1f554638d9ae
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:41:40 -0700

    Refactored into discrete functions with central interface.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,16 +1,43 @@
-
 import sys
 
-def ratings_output():
-    """ Reads given file, then spits out the ratings in alphabetical order by restaurant. 
-    """
+def zagat():
+    """ Function manages the zagat dictionary through a central interface."""
+    zagat_content = create_zagat()
+    format_zagat(zagat_content)
+    response = raw_input('Would you like to add an additional restaurant rating? Y/N\n').upper()
+    if response == "Y":
+        restaurant, score = add_restaurant_rating(zagat_content)
+        zagat_content[restaurant] = score
+        print format_zagat(zagat_content)
 
-    restaurant_data = open(sys.argv[1])
+
+def create_zagat():
+    """ Reads given file, then spits out the ratings in alphabetical order by restaurant."""
+
+    #restaurant_data = open(sys.argv[1])
     zagat = {}
-    for line in restaurant_data:
+    for line in open(sys.argv[1]):
         restaurant, score = line.rstrip().split(":")
         zagat[restaurant] = score
-    for restaurant in sorted(zagat):
-        print "{} is rated at {}.".format(restaurant, zagat[restaurant])
 
-ratings_output()
+    return zagat
+    
+def format_zagat(content):
+    """Sorts and prints the dictionary by alphabetical order"""
+
+    zagat_sorted = sorted(content)
+    for restaurant in zagat_sorted:
+        print "{} is rated at {}.".format(restaurant , content[restaurant])
+
+
+def add_restaurant_rating(rest_dictionary):
+    """Asks the user for a new restaurant and corresponding rating"""
+
+    new_restaurant = raw_input("Please give us the name of a restaurant not in our list: ").title()
+    new_score = int(raw_input("What score would you give to that restaurant, 1 - 5: "))
+    return [new_restaurant, new_score]
+
+    
+zagat()
+
+
